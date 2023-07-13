e404407c2bee556406a262b6fd9e3efd9196e9d5
Owen McComas <owen@mccomas.me>
2023-07-12 17:48:22 -0700

    little better

---
+++ src/components/ui/title.tsx
@@ -0,0 +1,40 @@
+import { useSession } from "next-auth/react";
+import { format } from "date-fns"
+import WeatherWidget from "./weather";
+import dynamic from "next/dynamic";
+
+const DynamicDigitalClock = dynamic(
+    () => import("./DigitalClock"),
+    {
+      ssr: false,
+    }
+  )
+
+export default function UserTitle() {
+    const user = useSession().data?.user;
+
+  if (user)
+    return (
+      <>
+        <div className="mx-auto flex w-full justify-between">
+          <div className="flex flex-col">
+            <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+              Good Morning {user?.name},
+              <br className="hidden sm:inline" />
+              This is your day at a glance.
+            </h1>
+            <p className="max-w-[700px] text-lg text-muted-foreground">
+              {format(Date.now(), "MMMM d, yyyy")}
+            </p>
+            <p className="text-md text-muted-foreground">
+              <DynamicDigitalClock/>
+            </p>
+          </div>
+          <div className="ml-auto">
+            <WeatherWidget />
+          </div>
+        </div>
+      </>
+    );
+  return <p className="text-white">Not Logged In</p>;
+}
